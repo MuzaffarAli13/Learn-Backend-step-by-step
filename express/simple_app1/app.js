fe62b5adcd7851a8e08a83564f78e13dace2037e
Muzaffar Ali <kalwar12khan@gmail.com>
2026-06-21 23:03:34 +0500

    post method

--- express/simple_app1/app.js
+++ express/simple_app1/app.js
@@ -1,12 +1,26 @@
-import express, { urlencoded } from 'express';
+import express from 'express';
 
 const PORT = 5000;
 const app = express();
 
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send('Hello from API');
 });
+
+// Post Method
+app.post("/api/v1/user",(req,res)=>{
+    const {name} = req.body;
+    console.log(name);
+    res.status(200).json({
+        message:`User data recived ${name}`,
+    });
+    
+});
+
+
+
 
 
 app.listen(PORT, () => {
